792621cec8c2232dc74970b3896cec99d3555905
Ng Kai Pong <kpng0@cse.cuhk.edu.hk>
2022-08-24 21:21:46 +0800

    Y. Mansar pretrain transformer: save weights with max validation accuracy

--- trsf_v2_genre_classification.py
+++ trsf_v2_genre_classification.py
@@ -1,5 +1,6 @@
 import json
 from glob import glob
+import os
 
 from sklearn.model_selection import train_test_split
 from tensorflow.keras.callbacks import ModelCheckpoint, ReduceLROnPlateau
@@ -13,12 +14,12 @@ if __name__ == "__main__":
     h5_name = "transformer_v2.h5"
     h5_pretrain = "transformer_pretrain.h5"
     batch_size = 32
-    epochs = 50
-    CLASS_MAPPING = json.load(open("/media/ml/data_ml/fma_metadata/mapping.json"))
-    id_to_genres = json.load(open("/media/ml/data_ml/fma_metadata/tracks_genre.json"))
+    epochs = 15
+    CLASS_MAPPING = json.load(open("data/fma_metadata/mapping.json"))
+    id_to_genres = json.load(open("data/fma_metadata/tracks_genre.json"))
     id_to_genres = {int(k): v for k, v in id_to_genres.items()}
 
-    base_path = "/media/ml/data_ml/fma_large"
+    base_path = os.path.join(os.getcwd(), 'data', os.listdir('data')[1], 'fma_large')
     files = sorted(list(glob(base_path + "/*/*.npy")))
     files = [x for x in files if id_to_genres[int(get_id_from_path(x))]]
     labels = [id_to_genres[int(get_id_from_path(x))] for x in files]
@@ -39,15 +40,15 @@ if __name__ == "__main__":
     if h5_pretrain:
         model.load_weights(h5_pretrain, by_name=True)
 
-
     checkpoint = ModelCheckpoint(
         h5_name,
-        monitor="val_loss",
+        monitor='val_accuracy',
+        mode='max',
         verbose=1,
         save_best_only=True,
-        mode="min",
         save_weights_only=True,
     )
+    
     reduce_o_p = ReduceLROnPlateau(
         monitor="val_loss", patience=20, min_lr=1e-7, mode="min"
     )
@@ -59,8 +60,5 @@ if __name__ == "__main__":
         ),
         epochs=epochs,
         callbacks=[checkpoint, reduce_o_p],
-        use_multiprocessing=True,
-        workers=12,
-        verbose=2,
         max_queue_size=64,
     )
