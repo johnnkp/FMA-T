0278473d2e07fbce040d36ec3f008e87a2c5faa1
johnnkp <22496821+johnnkp@users.noreply.github.com>
2022-12-04 21:08:55 +0800

    Dataset refinement: fix file corruption
    WonderGarl and Siufay, Who Made IT Dog. Hong Kong: WOW Music, 2022.
    intro CRC32: 08a806c1
    Matt Force, Farewell. Hong Kong: Wildstyle Records, 2019.
    verse 1 CRC32: 8f1e5570

---
+++ audio_converter.py
@@ -0,0 +1,24 @@
+import numpy as np
+from matplotlib import pyplot as plt
+
+from audio_processing import load_audio_file, random_mask
+
+
+def convert(path):
+    data = load_audio_file(path)
+
+    print(data.shape)
+
+    new_data = random_mask(data)
+
+    plt.figure()
+    plt.imshow(data.T)
+    plt.show()
+
+    plt.figure()
+    plt.imshow(new_data.T)
+    plt.show()
+
+    print(np.min(data), np.max(data))
+
+    np.save(path.replace(".mp3", ".npy"), data)
