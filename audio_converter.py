56689200c11146d37ec6d41bc212c28d777e691b
johnnkp <22496821+johnnkp@users.noreply.github.com>
2022-12-19 23:47:59 +0800

    Add random crop figure

--- audio_converter.py
+++ audio_converter.py
@@ -2,7 +2,7 @@
 import numpy as np
 from matplotlib import pyplot as plt
 
-from audio_processing import load_audio_file, random_mask
+from audio_processing import load_audio_file, random_mask, random_crop
 
 
 def convert(path):
@@ -15,11 +15,21 @@ def convert(path):
     plt.figure()
     plt.imshow(data.T)
     plt.show()
+    plt.savefig(path + ".png")
 
     plt.figure()
     plt.imshow(new_data.T)
     plt.show()
+    plt.savefig(path + "_random_mask.png")
 
     print(np.min(data), np.max(data))
 
     np.save(path.replace(".mp3", ".npy"), data)
+
+    crop_size = np.random.randint(128, 256)
+    crop = random_crop(np.load(path.replace(".mp3", ".npy")), crop_size=crop_size)
+
+    plt.figure()
+    plt.imshow(crop.T)
+    plt.show()
+    plt.savefig(path + "_random_crop.png")
